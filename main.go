2335351324472ee81ab3bdb40fa9dc123bf71311
Luiz Felipe Rosa <luizrosa8@proton.me>
2025-05-30 23:44:49 -0300

    adding more validations
    Signed-off-by: Luiz Felipe Rosa <luizrosa8@proton.me>

--- main.go
+++ main.go
@@ -1,21 +1,67 @@
 package main
 
 import (
+	"errors"
+	"fmt"
 	"os"
 
+	"github.com/charmbracelet/lipgloss"
 	"github.com/charmbracelet/log"
 	"github.com/urfave/cli/v2"
 )
 
-func buy(c *cli.Context) error {
-	stock := c.Args().Get(0)
-	log.Infof("Buying! %s\n", stock)
-	return nil
+var confirmStyle = lipgloss.NewStyle().Foreground(lipgloss.Color("%FFD700")).Bold(true)
+
+func ConfirmPrompt(message string) (bool, error) {
+	fmt.Print(confirmStyle.Render(message + " [y/N]:"))
+
+	var response string
+
+	fmt.Scanln(&response)
+
+	if response == "n" || response == "N" || response == "" {
+		return false, nil
+	} else {
+		if response == "y" || response == "Y" {
+			return true, nil
+		} else {
+			return false, errors.New("Unexpected answer")
+		}
+	}
 }
 
-func sell(c *cli.Context) error {
-	stock := c.Args().Get(0)
-	log.Infof("Selling %s\n", stock)
+func trade(c *cli.Context) error {
+	ticker := c.String("ticker")
+	qty := c.Float64("qty")
+	price := c.Float64("price")
+	currency := c.String("currency")
+
+	// guard function to handle errors
+	op := ""
+
+	if c.Bool("buy") {
+		op = "buy"
+	}
+	if c.Bool("sell") {
+		op = "sell"
+	}
+	if op == "" {
+		return errors.New("No valid operation, expected BUY or SELL")
+	}
+
+	prompt := fmt.Sprintf("Do you with to %s %f %s for  %f %s each?", op, qty, ticker, price, currency)
+
+	confirmation, err := ConfirmPrompt(prompt)
+	if err == nil {
+		return err
+	}
+
+	if confirmation {
+		log.Info("Done")
+	} else {
+		log.Warn("Not done!")
+	}
+
 	return nil
 }
 
@@ -24,14 +70,42 @@ func main() {
 	app := &cli.App{
 		Commands: []*cli.Command{
 			{
-				Name:    "buy",
-				Aliases: []string{"b"},
-				Action:  buy,
-			},
-			{
-				Name:    "sell",
-				Aliases: []string{"s"},
-				Action:  sell,
+				Name:    "trade",
+				Aliases: []string{"t"},
+				Usage:   "Register a trade operation (buy or sell)",
+				Flags: []cli.Flag{
+					&cli.StringFlag{
+						Name:     "ticker",
+						Usage:    "Name of the stock, e.g.: AAPL, BTC",
+						Required: true,
+					},
+					&cli.BoolFlag{
+						Name:    "buy",
+						Usage:   "Indicates that the operation is buy",
+						Aliases: []string{"b"},
+					},
+					&cli.BoolFlag{
+						Name:    "sell",
+						Usage:   "Indicates that the operation is sell",
+						Aliases: []string{"s"},
+					},
+					&cli.Float64Flag{
+						Name:     "qty",
+						Usage:    "Quantity of stocks",
+						Required: true,
+					},
+					&cli.Float64Flag{
+						Name:     "price",
+						Usage:    "Price of ticker",
+						Required: true,
+					},
+					&cli.StringFlag{
+						Name:     "currency",
+						Usage:    "Currency: BRL or USD",
+						Required: true,
+					},
+				},
+				Action: trade,
 			},
 		},
 	}
