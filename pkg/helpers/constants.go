9cfac7832f3aa6dafbf3c6a493df01378ea17d59
Luiz Felipe Fonseca Rosa <luizfelipefonsecarosa@gmail.com>
2025-07-06 19:28:38 -0300

    add function that returns the dollar price

--- pkg/helpers/constants.go
+++ pkg/helpers/constants.go
@@ -3,3 +3,9 @@ package helpers
 func GetLayoutDate() string {
 	return "2006-01-02"
 }
+
+// TODO: create a table of relations
+// an autoupdate here would be awesome, some kind of daemon
+func GetDollarInBRL() float64 {
+	return 5.55
+}
